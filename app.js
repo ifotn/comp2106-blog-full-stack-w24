242de22878f8d722538d56f5b1cb884c409248df
ifotn <Rich.Freeman@GeorgianCollege.ca>
2024-04-01 09:49:42 -0400

    l12/4 - cors enabled only in dev mode

--- app.js
+++ app.js
@@ -9,6 +9,14 @@ app.use(bodyParser.json());
 // db conn
 if (process.env.NODE_ENV !== 'production') {
     require('dotenv').config();
+    
+    // CORS in dev mode to accept requests from localhost:4200
+    // not needed in prod as angular app runs on same domain
+    const cors = require('cors');
+    app.use(cors({
+        origin: process.env.CLIENT_URL,
+        methods: 'GET,POST,PUT,DELETE,HEAD,OPTIONS'
+    }));
 }
 
 mongoose.connect(process.env.CONNECTION_STRING, {})
@@ -16,11 +24,7 @@ mongoose.connect(process.env.CONNECTION_STRING, {})
 .catch((err) => { console.log (`DB Connection Failed ${err}`); });
 
 // enable CORS for angular client app BEFORE controllers
-// const cors = require('cors');
-// app.use(cors({
-//     origin: process.env.CLIENT_URL,
-//     methods: 'GET,POST,PUT,DELETE,HEAD,OPTIONS'
-// }));
+
 
 // map routes
 const postsController = require('./controllers/posts');
